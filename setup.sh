34eae0ae943a03241997e238a4bcd18c8c5643c7
dashu15 <darshandineshsalunke@gmail.com>
2025-08-31 21:11:12 +0530

    Final setup for Render deployment

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Create Streamlit config
 mkdir -p ~/.streamlit/
 
 echo "\
@@ -12,3 +15,9 @@ enableCORS = false\n\
 enableXsrfProtection = false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+
+# ---- DOWNLOAD LARGE FILES FROM GOOGLE DRIVE ----
+# Replace <FILE_ID> with the actual Google Drive file ID
+
+# Example for similarity.pkl
+gdown --id 1rybXUErlPrdx1Y57z-P7yow1s9ZhjmjJ -O similarity.pkl
